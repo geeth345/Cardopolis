8657fafb7faa87b6dcf6945183b7a5dbc24ec13b
geeth345 <geethaartha.vagga@gmail.com>
2023-08-25 23:37:40 +0100

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+
+import "./styles/App.css";
+
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { Heading, Input, Button } from "@chakra-ui/react";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+    const [username, setUsername] = useState("");
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ChakraProvider>
+        <LoginMenu />
+    </ChakraProvider>
   )
 }
 
 export default App
+
+
+function LoginMenu( {username} ) {
+    return (
+        <div className="menu">
+            <Heading>Login</Heading>
+            <Input placeholder="Username" size="md" width="auto"/>
+            <CreateRoomButton />
+            <JoinRoomButton />
+        </div>
+    );
+}
+
+function CreateRoomButton() {
+    return (
+        <Button className="createRoomButton">Create Room</Button>
+    );
+}
+
+function JoinRoomButton() {
+    return (
+        <Button className="joinRoomButton">Join Room</Button>
+    );
+}
