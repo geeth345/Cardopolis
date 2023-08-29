813327fe5366c7cc33e36a06650af489e99b06cf
geeth345 <geethaartha.vagga@gmail.com>
2023-08-29 01:48:35 +0100

    basic client login form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,57 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
+
 
 import "./styles/App.css";
+import { connect, createRoom } from "./services/socketService.js";
 
-import { ChakraProvider } from '@chakra-ui/react';
+import {ChakraProvider} from '@chakra-ui/react';
 
-import { Heading, Input, Button } from "@chakra-ui/react";
+import {Heading, Input, Button} from "@chakra-ui/react";
+
+export default App
 
 function App() {
 
-    const [username, setUsername] = useState("");
+  const [username, setUsername] = useState("");
+  const handleUsernameChange = (e) => setUsername(e.target.value);
+
 
   return (
     <ChakraProvider>
-        <LoginMenu />
+      <div className="background"></div>
+      <LoginMenu user={username} setUser={handleUsernameChange}/>
     </ChakraProvider>
   )
 }
 
-export default App
 
-
-function LoginMenu( {username} ) {
-    return (
-        <div className="menu">
-            <Heading>Login</Heading>
-            <Input placeholder="Username" size="md" width="auto"/>
-            <CreateRoomButton />
-            <JoinRoomButton />
-        </div>
-    );
+function LoginMenu({user, setUser}) {
+  return (
+    <div className="menu">
+      <Heading>Login</Heading>
+      <Input onChange={setUser} placeholder="Username" size="md" width="auto"/>
+      <CreateRoomButton user={user}/>
+      <JoinRoomButton user={user}/>
+    </div>
+  );
 }
 
-function CreateRoomButton() {
-    return (
-        <Button className="createRoomButton">Create Room</Button>
-    );
+function CreateRoomButton({user}) {
+
+  function doCreateRoom() {
+    createRoom(999, user);
+  }
+
+  return (
+    <Button className="createRoomButton" onClick={doCreateRoom}>Create Room</Button>
+  );
 }
 
-function JoinRoomButton() {
-    return (
-        <Button className="joinRoomButton">Join Room</Button>
-    );
+function JoinRoomButton({user}) {
+  function doJoinRoom() {
+    connect(999, user);
+  }
+  return (
+    <Button className="joinRoomButton" onClick={doJoinRoom}>Join Room</Button>
+  );
 }
